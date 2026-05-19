974ffb903ea25d72dc2b1f000a0504e90d18eadd
xihucuyudaichi <zhenghanwei66@gmail.com>
2026-05-19 13:59:23 +0800

    Initial Commit

---
+++ app1/app1App.swift
@@ -0,0 +1,17 @@
+//
+//  app1App.swift
+//  app1
+//
+//  Created by 1111 on 2026/5/19.
+//
+
+import SwiftUI
+
+@main
+struct app1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
